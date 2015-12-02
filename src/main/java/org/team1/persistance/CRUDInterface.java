b9cb827ab264e9db1fa2015837d368d5ce9a16b9
qkrwogus88 <qkrwogus88@gmail.com>
2015-12-02 15:28:03 +0900

    123123

--- src/main/java/org/team1/persistance/CRUDInterface.java
+++ src/main/java/org/team1/persistance/CRUDInterface.java
@@ -3,4 +3,7 @@ package org.team1.persistance;
 public interface CRUDInterface<V,K> {
 
 	public void create(V vo) throws Exception;
+	public V read(K key) throws Exception;
+	public void update(V vo) throws Exception;
+	public void delete()
 }
