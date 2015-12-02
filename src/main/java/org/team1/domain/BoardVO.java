f9916b4ca490e47eb5b3712d3166d0317dfbf5b5
qkrwogus88 <qkrwogus88@gmail.com>
2015-12-02 18:49:53 +0900

    Setting
    jae1

--- src/main/java/org/team1/domain/BoardVO.java
+++ src/main/java/org/team1/domain/BoardVO.java
@@ -1,5 +1,64 @@
 package org.team1.domain;
 
+import java.sql.Timestamp;
+
 public class BoardVO {
 
+	private Integer bno;
+	private String title;
+	private String content;
+	private String writer;
+	private Timestamp regdate;
+	private Timestamp updatedate;
+	private Integer hit;
+	public Integer getBno() {
+		return bno;
+	}
+	public void setBno(Integer bno) {
+		this.bno = bno;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+	public String getWriter() {
+		return writer;
+	}
+	public void setWriter(String writer) {
+		this.writer = writer;
+	}
+	public Timestamp getRegdate() {
+		return regdate;
+	}
+	public void setRegdate(Timestamp regdate) {
+		this.regdate = regdate;
+	}
+	public Timestamp getUpdatedate() {
+		return updatedate;
+	}
+	public void setUpdatedate(Timestamp updatedate) {
+		this.updatedate = updatedate;
+	}
+	public Integer getHit() {
+		return hit;
+	}
+	public void setHit(Integer hit) {
+		this.hit = hit;
+	}
+	
+	@Override
+	public String toString() {
+		return "BoardVO [bno=" + bno + ", title=" + title + ", content=" + content + ", writer=" + writer + ", regdate="
+				+ regdate + ", updatedate=" + updatedate + ", hit=" + hit + "]";
+	}
+	
+	
 }
