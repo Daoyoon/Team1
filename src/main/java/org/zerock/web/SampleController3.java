7b36f809ec8469aea0d6604c605585929790d505
qkrwogus88 <qkrwogus88@gmail.com>
2015-12-02 14:51:29 +0900

    dodo>>nana
    jaehyun's 1st change

--- src/main/java/org/zerock/web/SampleController3.java
+++ src/main/java/org/zerock/web/SampleController3.java
@@ -20,7 +20,7 @@ public class SampleController3 {
 		MemberVO vo = new MemberVO();
         vo.setUserid("doyoon");
         vo.setUpw("user90");
-        vo.setUname("dodo");
+        vo.setUname("nana");
         vo.setEmail("wowo@naver.com");
         vo.setPhone("010-777-3943");
         vo.setGender("f");
