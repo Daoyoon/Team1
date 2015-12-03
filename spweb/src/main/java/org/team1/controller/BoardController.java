dd95102914324c333fe564611c6e44b7303ccc2b
qkrwogus88 <qkrwogus88@gmail.com>
2015-12-03 17:51:09 +0900

    boardController

--- spweb/src/main/java/org/team1/controller/BoardController.java
+++ spweb/src/main/java/org/team1/controller/BoardController.java
@@ -1,9 +1,5 @@
 package org.team1.controller;
 
-import java.sql.Date;
-import java.util.ArrayList;
-import java.util.List;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
