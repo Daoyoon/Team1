72bc6e8eb9c4d68cb693a8d9005b65decd2cfe1c
young-jun <leetop4432@gmail.com>
2015-12-03 13:49:39 +0900

    boardmapperimpl

---
+++ spweb/src/main/java/org/team1/persistence/BoardMapperImpl.java
@@ -0,0 +1,7 @@
+package org.team1.persistence;
+
+import org.team1.domain.BoardVO;
+
+public class BoardMapperImpl extends AbstractCRUDMapper<BoardVO, Integer> implements CRUDMapper<BoardVO, Integer> {
+
+}
